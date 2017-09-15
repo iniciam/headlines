f9f49830957e10be738de2c9a68a3495b1ed855b
iniciam <iniciam@gmail.com>
2017-09-15 18:33:02 +0800

    add jinja templates

---
+++ headlines.py
@@ -0,0 +1,19 @@
+import feedparser
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://rss.iol.io/iol/all-content-feed'}
+
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEED[publication])
+    return render_template("home.html", articles=feed['entries'])
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
